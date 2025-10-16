6e8a2f32cb919dc7b32c0556ee16a491700be75e
seoro <sho0814@hanyang.ac.kr>
2025-10-16 16:53:24 +0900

    sessiong following

--- contracts/MyERC20.sol
+++ contracts/MyERC20.sol
@@ -4,7 +4,15 @@ pragma solidity ^0.8.28;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyERC20 is ERC20 {
-    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
-        
+    constructor(string memory name_, string memory symbol_, uint256 amount) ERC20(name_, symbol_) {
+        _mint(msg.sender, amount);
+    }
+    
+    function mint(address account, uint256 amount) external {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) external {
+        _burn(account, amount);
     }
 }
